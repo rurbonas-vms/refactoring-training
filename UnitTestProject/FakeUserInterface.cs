ee8545a74d6e5c3567650ff821f2a852ea24f1a0
Robert Urbonas <robert.urbonas@varian.com>
2016-01-29 13:36:36 -0600

    Stub out a fake UI for testing purposes

---
+++ UnitTestProject/FakeUserInterface.cs
@@ -0,0 +1,25 @@
+﻿using Refactoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    public class FakeUserInterface : UserInterface
+    {
+        public void displayBanner(string bannerText)
+        {
+        }
+
+        public string getStringInputFromUser(string labelText)
+        {
+            return null;
+        }
+
+        public void promptUserToExit()
+        {
+        }
+    }
+}
