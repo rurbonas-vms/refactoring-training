b2748fb601c7e9e56ea952b7719b89ed80382d3c
Robert Urbonas <robert.urbonas@varian.com>
2016-01-29 13:16:35 -0600

    More preparation for breaking out the login code

---
+++ Refactoring/LoginManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring
+{
+    public class LoginManager
+    {
+        public LoginManager(List<User> users) // TODO: Make this an IEnumerable once we've changed the code to remove the indexing operators
+        {
+            if (users == null)
+                throw new ArgumentException();
+        }
+    }
+}
---
+++ UnitTestProject/TestLoginManager.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Refactoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class TestLoginManager
+    {
+        [Test]
+        [ExpectedException("System.ArgumentException")]
+        public void testLoginManagerCannotBeCreatedWithoutUserList()
+        {
+            LoginManager loginManager = new LoginManager(null);
+        }        
+    }
+}
